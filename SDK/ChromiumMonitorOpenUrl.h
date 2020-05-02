0c13783e66d4ce6f33a6756891965d7c1fe80ff7
tankaishuai <tankaishuai@126.com>
2020-05-02 16:06:15 +0800

    增加一个 SB实现的接口，仅用做对比！！

--- SDK/ChromiumMonitorOpenUrl.h
+++ SDK/ChromiumMonitorOpenUrl.h
@@ -15,6 +15,9 @@
 extern "C" __declspec(dllexport)
 HRESULT StartMonitorOpenUrlByNotifyWnd(HWND hNotifyWnd);
 
+extern "C" __declspec(dllexport)
+HRESULT StartMonitorOpenUrlByNotifyWnd_SB(HWND hNotifyWnd);
+
 //获取浏览器当前打开的Url
 extern "C" __declspec(dllexport)
 LPCSTR GetOpenedUrlByLParam(LPARAM lParam);
